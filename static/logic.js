a525eeae7084d600a86a3088f3ac007554859d41
Secreton <bharatchandra1586@gmail.com>
2026-03-25 17:14:49 +0530

    feat: add a simple canvas with simplistic color choice

---
+++ static/logic.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("Canvas");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = canvas.width;
+const CANVAS_HEIGHT = canvas.height;
+
+ctx.fillStyle = "#8F3C9E";
+ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
